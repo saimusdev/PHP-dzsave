d219af706cc54ea6ab5bdb114a4e523c8f04c15a
saimus <saimus.dev@gmail.com>
2014-11-23 19:00:49 +0100

    Simple test of openslide works. Dzsave not yet

--- test.c
+++ test.c
@@ -1,62 +1,61 @@
 /* compile with:
  *
- *      gcc -g -Wall try211.c `pkg-config vips --cflags --libs`
+ *      gcc -g -Wall test.c `pkg-config vips --cflags --libs` -o test
  */
 
 #include <stdio.h>
 #include <vips/vips.h>
+#include <vips/foreign.h>
 
 int
-main( int argc, char **argv )
+main(int argc, char **argv)
 {
 	GOptionContext *context;
 	GOptionGroup *main_group;
 	GError *error = NULL;
 	VipsImage *in;
-	double mean;
-	VipsImage *out;
 
-	if( VIPS_INIT( argv[0] ) ) {
-		vips_error_exit( NULL ); 
+	if(VIPS_INIT(argv[0])) {
+		vips_error_exit(NULL); 
 	}
 
-	context = g_option_context_new( "hello infile outfile - VIPS demo" );
+	context = g_option_context_new("simpyr inputSlide outputPyramidFolder - Pyramid builder");
 
-	main_group = g_option_group_new( NULL, NULL, NULL, NULL, NULL );
-	g_option_context_set_main_group( context, main_group );
-	g_option_context_add_group( context, vips_get_option_group() );
+	main_group = g_option_group_new(NULL, NULL, NULL, NULL, NULL);
+	g_option_context_set_main_group(context, main_group);
+	g_option_context_add_group(context, vips_get_option_group());
 
-	if( !g_option_context_parse( context, &argc, &argv, &error ) ) {
-		if( error ) {
-			fprintf( stderr, "%s\n", error->message );
-			g_error_free( error );
+	if(!g_option_context_parse(context, &argc, &argv, &error)) {
+		if(error) {
+			fprintf(stderr, "%s\n", error->message);
+			g_error_free(error);
 		}
 
-		vips_error_exit( NULL );
+		vips_error_exit(NULL);
 	}
 
-	if( argc != 3 )
-		vips_error_exit( "usage: %s infile outfile", argv[0] ); 
+//int vips_openslideload(const char *filename, VipsImage **out, ...)
+//int vips_dzsave(VipsImage *in, const char *name, ...)
+
+	if(argc < 3) {
+		vips_error_exit("usage: %s infile outfile dzsave-options...", argv[0]); 
+	}	
+
+	if(vips_openslideload(argv[1], &in, NULL) < 0) {
+		vips_error_exit(NULL);
+	}	
+
+	printf( "image width = %d\n", vips_image_get_width( in ) ); 	
+
+	if(vips_dzsave(in, argv[2], &argv[3]) < 0) {
+		vips_error_exit(NULL);
+	}	
+
+	printf("success");
+
 	
-	if( !(in = vips_image_new_from_file( argv[1], NULL )) )
-		vips_error_exit( NULL );
+	// Free Mem
+	g_object_unref(in); 
 
-	printf( "image width = %d\n", vips_image_get_width( in ) ); 
-
-	if( vips_avg( in, &mean, NULL ) )
-		vips_error_exit( NULL );
-
-	printf( "mean pixel value = %g\n", mean ); 
-
-	if( vips_invert( in, &out, NULL ) )
-		vips_error_exit( NULL );
-
-	g_object_unref( in ); 
-
-	if( vips_image_write_to_file( out, argv[2], NULL ) )
-		vips_error_exit( NULL );
-
-	g_object_unref( out ); 
-
-        return( 0 );
+	return 0;
 }
