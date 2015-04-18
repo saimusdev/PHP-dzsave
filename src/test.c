758af2188edf6d476a789af42ace250e7ee8c31f
saimus <saimus.dev@gmail.com>
2015-04-18 19:25:46 +0200

    Added 'vips_dzsave' call with named params

--- src/test.c
+++ src/test.c
@@ -10,8 +10,6 @@
 int
 main(int argc, char **argv)
 {
-	GOptionContext *context;
-	GOptionGroup *main_group;
 	GError *error = NULL;
 	VipsImage *in;
 
@@ -19,43 +17,34 @@ main(int argc, char **argv)
 		vips_error_exit(NULL); 
 	}
 
-	context = g_option_context_new("simpyr inputSlide outputPyramidFolder - Pyramid builder");
-
-	main_group = g_option_group_new(NULL, NULL, NULL, NULL, NULL);
-	g_option_context_set_main_group(context, main_group);
-	g_option_context_add_group(context, vips_get_option_group());
-
-	if(!g_option_context_parse(context, &argc, &argv, &error)) {
-		if(error) {
-			fprintf(stderr, "%s\n", error->message);
-			g_error_free(error);
-		}
-
-		vips_error_exit(NULL);
-	}
-
-//int vips_openslideload(const char *filename, VipsImage **out, ...)
-//int vips_dzsave(VipsImage *in, const char *name, ...)
-
 	if(argc < 3) {
 		vips_error_exit("usage: %s infile outfile dzsave-options...", argv[0]); 
 	}	
 
 	if(vips_openslideload(argv[1], &in, NULL) < 0) {
-		vips_error_exit(NULL);
+		vips_error_exit("could not open slide");
 	}	
 
-	printf( "image width = %d\n", vips_image_get_width( in ) ); 	
-
-	if(vips_dzsave(in, argv[2], &argv[3]) < 0) {
-		vips_error_exit(NULL);
-	}	
-
-	printf("success");
-
+	/*
+	VIPS_FOREIGN_DZ_LAYOUT_ZOOMIFY - Zoomify
+	VIPS_FOREIGN_DZ_LAYOUT_GOOGLE  - Google Maps
+	VIPS_FOREIGN_DZ_LAYOUT_DZ      - Deep Zoom
+	*/
+	if(vips_dzsave(in, argv[3],
+		"layout", VIPS_FOREIGN_DZ_LAYOUT_ZOOMIFY,
+		"suffix", ".jpeg",
+		"overlap", 0,
+		"tile_size", 256,
+		"depth", VIPS_FOREIGN_DZ_DEPTH_1PIXEL,
+		"centre", FALSE,
+		"angle", VIPS_ANGLE_0,
+		NULL)) {
+		vips_error_exit("could not save pyramid");
+	} 
 	
 	// Free Mem
 	g_object_unref(in); 
 
+	printf("success");
 	return 0;
 }
