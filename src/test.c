9401e302a377a89d04a78b1e7513ce0b8428b7ee
saimus <saimus.dev@gmail.com>
2015-04-18 21:09:48 +0200

    first successful test

--- src/test.c
+++ src/test.c
@@ -5,7 +5,7 @@
 int
 main(int argc, char **argv)
 {
-	GError *error = NULL;
+	///GError *error = NULL;
 	VipsImage *in;
 
 	if(VIPS_INIT(argv[0])) {
@@ -17,26 +17,24 @@ main(int argc, char **argv)
 		vips_error_exit("usage: %s infile outfile dzsave-options...", argv[0]); 
 	}	
 
-	printf("Argv 2: %s\n", argv[1]);
-	printf("Argv 3: %s\n", argv[2]);
-
 	if(vips_openslideload(argv[1], &in, NULL) < 0) {
 		vips_error_exit("could not open slide");
 	}	
 
-	printf( "image width = %d\n", vips_image_get_width( in ) ); 	
+	printf( "filename: %s\n", vips_image_get_filename(in));
+	printf( "widthxheight: %dx%d\n", vips_image_get_width(in), vips_image_get_height(in) ); 	
 
 	/*
 	VIPS_FOREIGN_DZ_LAYOUT_ZOOMIFY - Zoomify
 	VIPS_FOREIGN_DZ_LAYOUT_GOOGLE  - Google Maps
 	VIPS_FOREIGN_DZ_LAYOUT_DZ      - Deep Zoom
 	*/
-	if(vips_dzsave(in, argv[3],
+	if(vips_dzsave(in, argv[2],
 		"layout", VIPS_FOREIGN_DZ_LAYOUT_ZOOMIFY,
 		"suffix", ".jpeg",
 		"overlap", 0,
 		"tile_size", 256,
-		"depth", VIPS_FOREIGN_DZ_DEPTH_1PIXEL,
+		"depth", VIPS_FOREIGN_DZ_DEPTH_1TILE,
 		"centre", FALSE,
 		"angle", VIPS_ANGLE_0,
 		NULL)) {
